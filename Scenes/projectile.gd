d388a055bfe5cf09f3c7bac34e756b9a59524268
Erik <31732187+eln17011@users.noreply.github.com>
2024-04-13 18:49:32 +0200

    Tweaks

--- Scenes/projectile.gd
+++ Scenes/projectile.gd
@@ -1,19 +1,22 @@
 extends Node2D
 
-var speed:float = 200
-var proj_vect
+var speed:float = 100
+var proj_vect : Vector2
+var count = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	proj_vect = GlobalInfo.player.global_position - GlobalInfo.boss.global_position
-
+	
 func _physics_process(delta):
 	
-	global_position += proj_vect * delta
-
-
+	if proj_vect.length() < 70:
+		pass
+	if count % 100 == 0:
+		proj_vect = GlobalInfo.player.global_position - global_position
+	global_position += proj_vect.normalized() * speed * delta
+	count += count;
 func _on_area_2d_body_entered(body):
-	print("Launching attack")
 	GlobalInfo.boss.special_attack()
 	queue_free()
 
