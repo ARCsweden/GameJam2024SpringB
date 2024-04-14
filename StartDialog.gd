083cfb4a7e3432e48efe8ee37e1e694ff5cdfdac
William Forsberg <vilfor@hotmail.se>
2024-04-14 12:46:40 +0200

    Intro screen for game

---
+++ StartDialog.gd
@@ -0,0 +1,44 @@
+extends RichTextLabel
+
+@onready var boss_dialog = $"."
+@onready var timer = $TimerCharacter
+@onready var timeout = $Timeout
+
+var dialog = "" : set = _set_dialog
+var characters
+
+func _set_dialog(new_text):
+	boss_dialog.visible_characters = 0
+	characters = new_text.length()
+	boss_dialog.text = new_text
+	
+	if characters > 0:
+		timer.start()
+		boss_dialog.visible = true
+		pass
+	else:
+		boss_dialog.visible = false
+		pass
+		
+
+
+func _on_timer_timeout():
+	boss_dialog.visible_characters = boss_dialog.visible_characters + 1
+	timeout.stop()
+	timer.stop()
+	
+	if boss_dialog.visible_characters < characters:
+		timer.start()
+	else:
+		timeout.start()
+
+
+func _on_timeout_timeout():
+	boss_dialog.visible = false
+	get_tree().change_scene_to_file("res://game.tscn")
+	pass
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	dialog = "Det här är ett intro för spelet, Den inehåller story motivationer och är otroligt spännande. Faktiska spelet startar 10 sec efter det här är klart.........................................."
