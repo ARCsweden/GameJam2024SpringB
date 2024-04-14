0dde4655830a72f8605a3eb95464964d52220d87
William Forsberg <vilfor@hotmail.se>
2024-04-14 10:49:49 +0200

    Functioning BARS

--- UI/Bar.gd
+++ UI/Bar.gd
@@ -4,6 +4,7 @@ extends TextureProgressBar
 @onready var damage_Bar = $Damage
 
 var attrib = 0 : set = _set_attrib
+var attrib_max = 0 : set = _set_attrib_max
 
 func _set_attrib(new_attrib):
 	var prev_attrib = attrib
@@ -15,12 +16,11 @@ func _set_attrib(new_attrib):
 	else:
 		damage_Bar.value = attrib
 
-func init_attrib(_value, _max):
-	attrib = _value
-	value = attrib
-	max_value = _max
-	damage_Bar.max_value = _max
-	damage_Bar.value = attrib
+func _set_attrib_max(max_attrib):
+	max_value = max_attrib
+	damage_Bar.max_value = max_attrib
+	
+	attrib = max_attrib
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
